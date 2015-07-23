4af8a452176a06440c3aa16ea8f28e6d2310fa93
Kevin Pruett <pruett.kevin@gmail.com>
2015-07-23 16:44:31 -0400

    bootstrap pages

---
+++ src/assets/javascripts/build.js
@@ -0,0 +1,13 @@
+import './modules/rAF'
+import utils from './modules/utils'
+
+const header = document.querySelector('[data-hook="getHeader"]');
+const heroContent = document.querySelector('[data-hook="getHeroContent"]');
+
+const animateElements = function() {
+  requestAnimationFrame(() => utils.addClass(header, '-anim_appear'));
+  //requestAnimationFrame(() => utils.addClass(heroContent, '-anim_appear'));
+}
+
+setTimeout(() => requestAnimationFrame(animateElements), 500);
+
