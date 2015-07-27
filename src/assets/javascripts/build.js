a752a38d727165bd965c4d3118d4dfa563d4e2ca
Kevin Pruett <pruett.kevin@gmail.com>
2015-07-27 11:57:56 -0400

    build styles

--- src/assets/javascripts/build.js
+++ src/assets/javascripts/build.js
@@ -4,6 +4,7 @@ import noUiSlider from './modules/nouislider.min';
 
 const header = document.querySelector('[data-hook="getHeader"]');
 const heroContent = document.querySelector('[data-hook="getHeroContent"]');
+const budgetRange = document.querySelector('[data-hook="budgetRange"]');
 
 const animateElements = function() {
   requestAnimationFrame(() => utils.addClass(header, '-anim_appear'));
@@ -11,10 +12,12 @@ const animateElements = function() {
 
 setTimeout(() => requestAnimationFrame(animateElements), 500);
 
-noUiSlider.create(document.querySelector('[data-hook="budgetRange"]'), {
+noUiSlider.create(budgetRange), {
   start: [ 10000, 20000 ],
   range: { 'min': [ 5000 ],'max': [ 50000 ] },
   step: 1000,
   margin: 2000,
   connect: true
-});
+};
+
+console.log('hello');
