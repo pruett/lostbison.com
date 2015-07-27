49067c43f9ede1e54eaea99feffa86de9d97e260
Kevin Pruett <pruett.kevin@gmail.com>
2015-07-27 17:37:11 -0400

    dynamic budget range

--- src/assets/javascripts/build.js
+++ src/assets/javascripts/build.js
@@ -5,19 +5,34 @@ import noUiSlider from './modules/nouislider.min';
 const header = document.querySelector('[data-hook="getHeader"]');
 const heroContent = document.querySelector('[data-hook="getHeroContent"]');
 const budgetRange = document.querySelector('[data-hook="budgetRange"]');
+const budgetFrom = document.querySelector('[data-hook="budgetFrom"]');
+const budgetTo = document.querySelector('[data-hook="budgetTo"]');
 
 const animateElements = function() {
   requestAnimationFrame(() => utils.addClass(header, '-anim_appear'));
 }
 
+noUiSlider.create(budgetRange, {
+  start: [ 10000, 20000 ],
+  margin: 5000,
+  range: {
+    'min': 5000,
+    'max': 50000
+  },
+  step: 1000,
+  connect: true,
+  format: {
+    to: function(value) {
+      return '$' + value.toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+    from: Number
+  }
+});
+
 setTimeout(() => requestAnimationFrame(animateElements), 500);
 
-noUiSlider.create(budgetRange), {
-  start: [ 10000, 20000 ],
-  range: { 'min': [ 5000 ],'max': [ 50000 ] },
-  step: 1000,
-  margin: 2000,
-  connect: true
-};
-
-console.log('hello');
+budgetRange.noUiSlider.on('update', function(values) {
+  budgetFrom.innerText = values[0];
+  budgetTo.innerText = values[1] === "$50,000" ? "$50,000+" : values[1];
+  return false;
+});
