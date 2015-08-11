5c59455d07c594c035696423c381d4ac4baef5b1
Kevin Pruett <pruett.kevin@gmail.com>
2015-08-11 12:42:13 -0400

    dynamic blog composition

--- gulpTasks/blog/jade.js
+++ gulpTasks/blog/jade.js
@@ -2,14 +2,4 @@ var gulp    = require('gulp'),
     connect = require('gulp-connect'),
     jade    = require('gulp-jade');
 
-gulp.task('blog:jade:compile', function() {
-  return gulp.src(['./src/blog/views/*.jade'])
-    .pipe(jade({
-      locals: require('../../src/data'),
-      pretty: false
-    }))
-    .pipe(gulp.dest('./build/blog'))
-    .pipe(connect.reload());
-});
-
-gulp.task('blog:jade', ['blog:jade:compile', 'blog:posts']);
+gulp.task('blog:jade', ['blog:posts', 'blog:index']);
--- gulpTasks/blog/posts.js
+++ gulpTasks/blog/posts.js
@@ -1,41 +1,84 @@
-var gulp         = require('gulp'),
-    connect      = require('gulp-connect'),
-    path         = require('path'),
-    moment       = require('moment'),
-    rename       = require('gulp-rename'),
-    data         = require('gulp-data'),
-    fm           = require('front-matter'),
-    templateData = require('../../src/data'),
-    jade         = require('gulp-jade');
+var gulp     = require('gulp'),
+    connect  = require('gulp-connect'),
+    path     = require('path'),
+    moment   = require('moment'),
+    foreach  = require('gulp-foreach'),
+    rename   = require('gulp-rename'),
+    data     = require('gulp-data'),
+    fm       = require('front-matter'),
+    allPosts = [],
+    jade     = require('gulp-jade');
 
-var getDateFromFileName = function(path) {
-  //regex this shit
-  //return YYYY-MM-DD
-};
+var fileNameRegex = /^(\d{4})-(\d{2})-(\d{2})-([\w|-]*)/i;
 
-var formatDate = function(date) {
-  //return moment(date).format('MMM Do, YYYY'));
+var parseFileName = function(fileName) {
+  var match = fileNameRegex.exec(fileName)
+
+  return {
+    year: match[1],
+    month: match[2],
+    day: match[3],
+    post: match[4]
+  }
 };
 
 gulp.task('blog:posts', function() {
+  var templateData = require('../../src/data');
+
   gulp.src('./src/blog/views/posts/*.jade')
     .pipe(data(function(file) {
-      //console.log(path.basename(file.path, '.jade');
+      var fileName = path.basename(file.path, '.jade')
       var content = fm(String(file.contents));
+      var postInfo = parseFileName(fileName);
+
       file.contents = new Buffer(content.body);
-      //console.log(content.attributes);
-      //content.attributes.attributes
-      // augment generic data to include post frontmatter
+      // augment local data to include post frontmatter
       templateData.post = content.attributes
-      console.log(getDateFromFileName(path.basename(file.path, '.jade')));
-      //templateData.post.url = something
-      //templateData.post.convertedDate = 
+      templateData.post.convertedDate = moment(postInfo.year+'-'+postInfo.month+'-'+postInfo.day).format('MMM Do, YYYY');
+      templateData.dirname = postInfo.year+'/'+postInfo.month+'/'+postInfo.day;
+      templateData.basename = postInfo.post;
+    }))
+    .pipe(jade({
+      locals: templateData,
+      pretty: false
     }))
     .pipe(rename(function(path) {
-      // rename to 2015/08/06/name-of-post.jade
-      console.log(path);
-      console.log(templateData.post.date);
+      path.dirname = templateData.dirname;
+      path.basename = templateData.basename;
     }))
+    .pipe(gulp.dest('./build/blog'))
+    .pipe(connect.reload());
+});
+
+gulp.task('blog:compile:posts', function() {
+  allPosts = [];
+
+  gulp.src('./src/blog/views/posts/*.jade')
+    .pipe(data(function(file) {
+      var fileName = path.basename(file.path, '.jade')
+      var content = fm(String(file.contents));
+      var postInfo = parseFileName(fileName);
+
+      file.contents = new Buffer(content.body);
+
+      var post = {
+        meta: content.attributes,
+        convertedDate: moment(postInfo.year+'-'+postInfo.month+'-'+postInfo.day).format('MMM Do, YYYY'),
+        dirname: postInfo.year+'/'+postInfo.month+'/'+postInfo.day,
+        basename: postInfo.post
+      };
+
+      allPosts.push(post);
+    }));
+
+  return false;
+});
+
+gulp.task('blog:index', ['blog:compile:posts'], function() {
+  var templateData = require('../../src/data');
+  templateData.posts = allPosts;
+
+  return gulp.src('./src/blog/views/index.jade')
     .pipe(jade({
       locals: templateData,
       pretty: false
@@ -43,19 +86,3 @@ gulp.task('blog:posts', function() {
     .pipe(gulp.dest('./build/blog'))
     .pipe(connect.reload());
 });
-
-//gulp.task('blog:index', function() {
-  //gulp.src('./src/blog/views/posts/*.jade')
-    //.pipe(data(function(file) {
-      //var content = fm(String(file.contents));
-      //file.contents = new Buffer(content.body);
-      //// augment generic data to include post frontmatter
-      //templateData.post = content.attributes
-    //}))
-    //.pipe(jade({
-      //locals: templateData,
-      //pretty: false
-    //}))
-    //.pipe(gulp.dest('./build/blog'))
-    //.pipe(connect.reload());
-//});
